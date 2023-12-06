153fd75c1d9f883bc2dbfd95a747e43b98ea05e8
TheTagEnd <vashudev8777@gmail.com>
2023-12-06 21:51:43 +0530

    done some security stuffs

--- amazon_clone/server/index.js
+++ amazon_clone/server/index.js
@@ -4,15 +4,17 @@ const app = express() ;
 const mongoose =  require('mongoose');
 const authRouter = require('./routes/auth');
 const adminRouter  =  require('./routes/admin');
-const DB ="mongodb+srv://vashudev8777:1111111111@cluster0.gnfdlhi.mongodb.net/?retryWrites=true&w=majority";
+
+const DB  =  require('./secret');
 app.use(express.json());
 app.use(authRouter);
 app.use(adminRouter);
 mongoose.connect(DB).then( () => {
- console.log("connect");
+console.log("connect");
 }).catch((e) => {
   console.log(e);
 });
 app.listen(PORT , "0.0.0.0", () => {
   console.log(`connected at ${PORT}`);
 });
+
