0855d07f7466565d3100e3d7889400b30a4df16f
TheTagEnd <vashudev8777@gmail.com>
2023-12-07 17:20:38 +0530

    category  deals screen added

---
+++ amazon_clone/lib/features/home/screens/category_deals_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryDealsScreen extends StatefulWidget {
+  const CategoryDealsScreen({super.key});
+
+  @override
+  State<CategoryDealsScreen> createState() => _CategoryDealsScreenState();
+}
+
+class _CategoryDealsScreenState extends State<CategoryDealsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
